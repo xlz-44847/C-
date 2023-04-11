f348523876e141fb42c855d02d62992b342172fa
xlz-44847 <1940629384@qq.com>
2023-04-12 00:53:30 +0800

    commit code for 4 file

---
+++ code for 4/function 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int add(int x,int y)
+{
+    int z = x+y;
+    return z;
+}
+int main()
+{
+    int a=0,b=0,c=0;
+    scanf("%d%d",&a,&b);
+    c=add(a,b);
+    printf("%d\n",c);
+    return 0;
+}
