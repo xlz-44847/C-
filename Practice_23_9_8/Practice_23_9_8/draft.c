1a8c3d45f8e083b7dc02c25cefb6cef46a0082b9
xlz-44847 <1940629384@qq.com>
2023-09-08 22:08:02 +0800

    今日晚饭后刷题测试代码。

---
+++ Practice_23_9_8/Practice_23_9_8/draft.c
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+int Comp(int num1, int num2)
+{
+	return (num1 > num2 ? num1 : num2);
+}
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d", Comp(a, b));
+	return 0;
+}
--- Practice_23_9_8/Practice_23_9_8/test.c
+++ Practice_23_9_8/Practice_23_9_8/test.c
@@ -1,16 +1,16 @@
-#define _CRT_SECURE_NO_WARNINGS 
-#include <stdio.h>
-
-int add(int x, int y)
-{
-    int z = x + y;
-    return z;
-}
-int main()
-{
-    int a = 0, b = 0, c = 0;
-    scanf("%d%d", &a, &b);
-    c = add(a, b);
-    printf("%d\n", c);
-    return 0;
-}
+﻿//#define _CRT_SECURE_NO_WARNINGS 
+//#include <stdio.h>
+//
+//int add(int x, int y)
+//{
+//    int z = x + y;
+//    return z;
+//}
+//int main()
+//{
+//    int a = 0, b = 0, c = 0;
+//    scanf("%d%d", &a, &b);
+//    c = add(a, b);
+//    printf("%d\n", c);
+//    return 0;
+//}
